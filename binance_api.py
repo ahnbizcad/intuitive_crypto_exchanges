84cc9a53afe83a7b00d2fb1c23e31dee04f5505a
Jeehoo Ahn <ahnbizcad@gmail.com>
2018-01-14 04:50:33 +0000

    made binance get trade history call working. separated call from executing script

---
+++ binance_api.py
@@ -0,0 +1,45 @@
+# utilities
+import sys
+import requests, urllib
+import json, yaml
+import time
+from datetime import datetime
+# cryptography
+import hmac, hashlib
+# display
+import matplotlib.pyplot as mplpyplot
+# analysis
+import numpy
+import pandas
+# import keras
+
+def get_trade_history(exchange, key_api_public, key_api_secret, coin_base, coin_quote):
+    if exchange == 'binance':
+        url = 'https://api.binance.com/api/v3/myTrades'
+
+        headers = {'X-MBX-APIKEY' : key_api_public}
+
+        timestamp = int(time.time()*1000)
+        payload = {
+        'timestamp':timestamp,
+        'symbol': coin_base + coin_quote,
+        # 'fromId': 1, <- binance entry id
+        }
+        querystring = urllib.parse.urlencode(payload)
+        signature = hmac.new(
+            key_api_secret.encode('utf-8'),
+            querystring.encode('utf-8'),
+            hashlib.sha256,
+        ).hexdigest()
+
+        payload['signature'] = signature
+        r = requests.get(url, headers=headers, params=payload)
+        j = json.loads(r.text)
+
+        # add human readable field for time for the version passed for display.
+        j_pandas = j
+        for i, trade in enumerate(j):
+            j_pandas[i]['time_utc'] = datetime.fromtimestamp(int(trade['time']/1000))
+
+        df = pandas.DataFrame.from_dict(j_pandas)
+        return df
--- run.py
+++ run.py
@@ -1,12 +1,27 @@
-import requests
-import json
+# utilities
+import sys
+import requests, urllib
+import json, yaml
+import time
+from datetime import datetime
+# cryptography
+import hmac, hashlib
+# display
+import matplotlib.pyplot as mplpyplot
+# analysis
+import numpy
+import pandas
 
-def get_trade_history():
-    url = 'http://google.com'
-    resp = requests.get(url)
-
-    return resp.text
+# local app files
+import binance_api
 
 
-text = get_trade_history()
+with open(".credentials.yaml", 'r') as credentials_yaml_file:
+    credentials_dict = yaml.load(credentials_yaml_file)
+
+    key_api_binance_public = credentials_dict['exchanges']['binance']['public']
+    key_api_binance_secret = credentials_dict['exchanges']['binance']['secret']
+
+
+text = binance_api.get_trade_history('binance', key_api_binance_public, key_api_binance_secret, 'LTC', 'BTC')
 print(text)
